2225d3133384c2b27a871a536076bb7556091d4f
Deadlye <kevin.rothenbuhler@gmail.com>
2021-08-18 11:41:34 +0200

    remaining of the UI

--- app/src/main/java/ch/kra/mycellar/ItemDetailsFragment.kt
+++ app/src/main/java/ch/kra/mycellar/ItemDetailsFragment.kt
@@ -1,4 +1,38 @@
 package ch.kra.mycellar
 
-class ItemDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import ch.kra.mycellar.databinding.FragmentItemDetailsBinding
+
+class ItemDetailsFragment : Fragment() {
+    private var _binding: FragmentItemDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentItemDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //set the value of the dropdownList
+        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        binding.dropDownType.setAdapter(adapter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/ch/kra/mycellar/TypeSelectionFragment.kt
+++ app/src/main/java/ch/kra/mycellar/TypeSelectionFragment.kt
@@ -1,4 +1,32 @@
 package ch.kra.mycellar
 
-class TypeSelectionFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ch.kra.mycellar.databinding.FragmentStartBinding
+
+class TypeSelectionFragment: Fragment() {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
