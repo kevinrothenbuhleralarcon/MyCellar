836fcbe1276d33a7fe925ed238ebbb696e6d9ccb
Deadlye <kevin.rothenbuhler@gmail.com>
2021-12-19 21:07:45 +0100

    add image for wine type background, start to replace livedata by stateflow, continuation of wine list screen

--- app/src/main/java/ch/kra/mycellar/ui/WineListScreen.kt
+++ app/src/main/java/ch/kra/mycellar/ui/WineListScreen.kt
@@ -1,8 +1,9 @@
 package ch.kra.mycellar.ui
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -11,15 +12,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import ch.kra.mycellar.R
+import ch.kra.mycellar.database.Wine
+import ch.kra.mycellar.ui.viewmodel.WineViewModel
 
 @Composable
 fun WineListScreen(
@@ -31,11 +37,7 @@ fun WineListScreen(
             .padding(16.dp)
     ) {
         WineListScreenToolbar(navigate = navigate)
-        WineCard(
-            onClick = navigate,
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-        )
+        WineList(onClick = navigate)
     }
 }
 
@@ -72,28 +74,96 @@ private fun WineListScreenToolbar(
 }
 
 @Composable
+private fun WineList(
+    viewModel: WineViewModel = hiltViewModel(),
+    onClick: (Int) -> Unit
+) {
+    val wineList by remember {
+        viewModel.listWine
+    }
+    LazyColumn(contentPadding = PaddingValues(8.dp)) {
+        val itemCount = if (wineList.size % 2 == 0) {
+            wineList.size / 2
+        } else {
+            wineList.size / 2 +1
+        }
+        items(itemCount) {
+            WineListRow(rowNumber = it, wineList = wineList, onClick = onClick)
+        }
+    }
+}
+
+@Composable
+private fun WineListRow(
+    rowNumber: Int,
+    wineList: List<Wine>,
+    modifier: Modifier = Modifier,
+    onClick: (Int) -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        WineCard(
+            wine = wineList[rowNumber * 2],
+            painter = painterResource(id = R.drawable.red_wine),
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .height(150.dp)
+        )
+
+        if (wineList.size >= rowNumber * 2 + 2) {
+            WineCard(
+                wine = wineList[rowNumber * 2 + 1],
+                painter = painterResource(id = R.drawable.white_wine),
+                onClick = onClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+            )
+        } else {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+
+
+    }
+}
+
+@Composable
 private fun WineCard(
+    wine: Wine,
+    painter: Painter,
     modifier: Modifier = Modifier,
     onClick: (Int) -> Unit
 ) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
-            .shadow(5.dp, RoundedCornerShape(10.dp))
-            .clip(RoundedCornerShape(10.dp))
-            .background(
-                Brush.verticalGradient(
-                    listOf(
-                        Color.Red,
-                        Color.White
-                    )
-                )
-            )
+            .padding(8.dp)
             .clickable { onClick(1) }
     ) {
-        Column {
-            Text(text = "Pino Gris")
-            Text(text = "Didier")
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.3f,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(10.dp))
+        )
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(text = wine.wineName)
+            Text(text = wine.offeredBy)
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically,
@@ -110,7 +180,7 @@ private fun WineCard(
                             /* TODO add wine */
                         }
                 )
-                Text(text = "50")
+                Text(text = wine.quantity.toString())
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_remove_24),
                     contentDescription =null,
--- app/src/main/java/ch/kra/mycellar/ui/viewmodel/WineViewModel.kt
+++ app/src/main/java/ch/kra/mycellar/ui/viewmodel/WineViewModel.kt
@@ -1,10 +1,14 @@
 package ch.kra.mycellar.ui.viewmodel
 
-import androidx.lifecycle.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import ch.kra.mycellar.WineType
 import ch.kra.mycellar.database.Wine
 import ch.kra.mycellar.reposotories.WineRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,17 +17,27 @@ class WineViewModel @Inject constructor(
     private val wineRepository: WineRepository
 ) : ViewModel() {
 
-    private var _wineType = MutableLiveData(WineType.ALL.resId)
-    val wineType: LiveData<Int> get() = _wineType
-    val listWine: LiveData<List<Wine>> = Transformations.switchMap(_wineType) { _ -> getList() }
+    private var _wineType = mutableStateOf(WineType.ALL.resId)
+    val wineType get() = _wineType
+    var listWine = mutableStateOf<List<Wine>>(listOf())
+    //val listWine: LiveData<List<Wine>> = Transformations.switchMap(_wineType) { _ -> getList() }
+
+    init {
+        changeWineType(WineType.ALL.resId)
+    }
 
     fun changeWineType(wineType: Int) {
         _wineType.value = wineType
+        getList()
     }
 
-    fun getWine(wineId: Int): LiveData<Wine> {
-        return wineRepository.getWine(wineId).asLiveData()
+    fun getWine(wineId: Int): Flow<Wine> {
+        return wineRepository.getWine(wineId)
     }
+
+    /*fun getWine(wineId: Int): LiveData<Wine> {
+        return wineRepository.getWine(wineId).asLiveData()
+    }*/
 
     fun isEntryValid(wineName: String, wineType: String, quantity: String): Boolean {
         if (wineName.isBlank() || wineType.isBlank() || quantity.isBlank() || quantity.toIntOrNull() == null) {
@@ -60,13 +74,29 @@ class WineViewModel @Inject constructor(
         }
     }
 
-    private fun getList(): LiveData<List<Wine>> {
+    private fun getList() {
+        listWine.value = listOf()
+        viewModelScope.launch {
+            if (wineType.value == WineType.ALL.resId) {
+                wineRepository.getAllWine().collect {
+                listWine.value += it
+                }
+            } else {
+                wineRepository.getWineByType(wineType.value!!).collect {
+                    listWine.value += it
+                }
+            }
+        }
+
+    }
+
+    /*private fun getList(): LiveData<List<Wine>> {
         return if (wineType.value == WineType.ALL.resId) {
             wineRepository.getAllWine().asLiveData()
         } else {
             wineRepository.getWineByType(wineType.value!!).asLiveData()
         }
-    }
+    }*/
 
     private fun getNewWineEntry(wineName: String, wineType: Int, quantity: String, offeredBy: String): Wine {
         return Wine(
