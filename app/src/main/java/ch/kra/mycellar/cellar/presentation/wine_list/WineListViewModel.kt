5a19d03c533cc5ecb2d7524f2a7f654883e5b9bd
Deadlye <kevin.rothenbuhler@gmail.com>
2022-08-04 11:42:52 +0200

    Update the WineListViewModel to add the created use_case, use the new UIEvent and WineListEvent

--- app/src/main/java/ch/kra/mycellar/cellar/presentation/wine_list/WineListViewModel.kt
+++ app/src/main/java/ch/kra/mycellar/cellar/presentation/wine_list/WineListViewModel.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import ch.kra.mycellar.cellar.data.local.entity.Wine
 import ch.kra.mycellar.cellar.domain.WineType
 import ch.kra.mycellar.cellar.domain.repository.WineRepository
+import ch.kra.mycellar.cellar.domain.use_case.IncreaseDecreaseWineQuantity
 import ch.kra.mycellar.core.DispatcherProvider
+import ch.kra.mycellar.core.Route
+import ch.kra.mycellar.core.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,14 +21,24 @@ import javax.inject.Inject
 @OptIn(ExperimentalCoroutinesApi::class)
 class WineListViewModel @Inject constructor(
     private val wineRepository: WineRepository,
+    private val increaseDecreaseWineQuantity: IncreaseDecreaseWineQuantity,
     private val dispatcher: DispatcherProvider
 ) : ViewModel() {
 
     private val wineType = MutableStateFlow(WineType.ALL)
-    private val listWine = wineType.flatMapLatest { getList(it) }
+    private val listWine = wineType.flatMapLatest { wineType ->
+        if (wineType == WineType.ALL) {
+            wineRepository.getAllWine().flowOn(dispatcher.io)
+        } else {
+            wineRepository.getWineByType(wineType).flowOn(dispatcher.io)
+        }
+    }
 
     private val _wineListState = mutableStateOf(WineListState())
     val wineListState: State<WineListState> = _wineListState
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
 
     init {
         wineType.combine(listWine) { wineType, listWine ->
@@ -36,49 +49,41 @@ class WineListViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    fun changeWineType(newType: WineType) {
-        wineType.value = newType
-    }
+    fun onEvent(event: WineListEvent) {
+        when (event) {
+            is WineListEvent.WineTypeSelectionClicked -> {
+                _wineListState.value = wineListState.value.copy(
+                    isWineTypeExpanded = !wineListState.value.isWineTypeExpanded
+                )
+            }
 
-    private fun getList(wineType: WineType): Flow<List<Wine>> {
-        return if (wineType == WineType.ALL) {
-            wineRepository.getAllWine().flowOn(dispatcher.io)
-        } else {
-            wineRepository.getWineByType(wineType).flowOn(dispatcher.io)
+            is WineListEvent.WineTypeSelected -> {
+                wineType.value = event.wineType
+            }
+
+            is WineListEvent.WineSelected -> {
+                sendUIEvent(UIEvent.Navigate(
+                    route = "${Route.WINE_DETAIL_SCREEN}?${Route.Arguments.WINE_ID}=${event.wineId}"
+                ))
+            }
+
+            is WineListEvent.ChangeQuantity -> {
+                viewModelScope.launch(dispatcher.io) {
+                    increaseDecreaseWineQuantity(event.wine, event.isAdd)
+                }
+            }
+
+            is WineListEvent.AddNewWine -> {
+                sendUIEvent(UIEvent.Navigate(
+                    route = Route.WINE_DETAIL_SCREEN
+                ))
+            }
         }
     }
 
-    fun changeQuantity(wine: Wine, add: Boolean) {
-        if (!(wine.quantity == 0 && !add )) {
-            var quantityChanged = -1
-            if (add) { quantityChanged = 1 }
-            updateWine(
-                wine.id,
-                wine.wineName,
-                wine.wineType,
-                (wine.quantity + quantityChanged).toString(),
-                wine.offeredBy
-            )
-        }
-    }
-
-    private fun getUpdateWineEntry(wineId: Int, wineName: String, wineType: WineType, quantity: String, offeredBy: String?): Wine {
-        return Wine(
-            id = wineId,
-            wineName = wineName,
-            wineType = wineType,
-            quantity = quantity.toInt(),
-            offeredBy = offeredBy
-        )
-    }
-
-    private fun updateWine(wineId: Int, wineName: String, wineType: WineType, quantity: String, offeredBy: String?){
-        updateWine(getUpdateWineEntry(wineId, wineName, wineType, quantity, offeredBy))
-    }
-
-    private fun updateWine(wine: Wine) {
+    private fun sendUIEvent(event: UIEvent) {
         viewModelScope.launch {
-            wineRepository.update(wine)
+            _uiEvent.send(event)
         }
     }
 }
